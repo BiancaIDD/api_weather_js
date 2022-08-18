8ded4e43fab51749284dff4a488dd70e48bd6f72
bianca <cupcakeeeee17@gmail.com>
2022-08-18 15:58:46 -0600

    ajuste de https

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 async function fetchWeather(countryCode, city) {
   const myApiKey = "d5012fb4f7ae03546323e8025ffbc073";
   let response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&APPID=${myApiKey}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&APPID=${myApiKey}&units=metric`
   );
   let weather = await response.json();
   return weather;
 }
 
 async function showWeather() {
-  let country = document.getElementById("pais");
+  let country = document.getElementById("country");
   let countryCode = country.options[country.selectedIndex].value;
-  let city = document.getElementById("ciudad").value;
+  let city = document.getElementById("city").value;
 
   let weather = await fetchWeather(countryCode, city);
-  let nombreCiudad = document.getElementById("nombre_ciudad");
+  let nombreCiudad = document.getElementById("city_name");
   let img = document.getElementById("img");
   let temperature = document.getElementById("temperature");
   let temperatureMax = document.getElementById("temperature_max");
   let temperatureMin = document.getElementById("temperature_min");
 
   nombreCiudad.innerHTML = weather["name"];
-  const iconUrl = `http://openweathermap.org/img/wn/${weather["weather"][0]["icon"]}@2x.png`;
+  const iconUrl = `https://openweathermap.org/img/wn/${weather["weather"][0]["icon"]}@2x.png`;
   img.src = iconUrl;
   temperature.innerHTML = weather["main"]["temp"] + " °C";
   temperatureMax.innerHTML = "Max: " + weather["main"]["temp_max"] + " °C";
